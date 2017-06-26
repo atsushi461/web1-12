90e5f88c8007ba808dbcbcd036a3b517428ab511
atsushi461 <g1627030@st.t-kougei.ac.jp>
2017-06-26 18:06:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,29 +1,27 @@
    
-    for(var i =1; i <102; i++)
+    for(var i =1; i <101; i++)
   {
     var li = document.createElement('li');
-    var num = -1+i;
-    li.textContent = num;
+    li.textContent = i;
 
      
      
     
-    if(num%15==0)
+    if(i%15==0)
     {
      li.textContent = 'FizzBuzz';
     
        
     }
-    else if(num%3==0)
+    else if(i%3==0)
     {
      li.textContent = 'Fizz';
     
        
     }
-    else if(num%5==0)
+    else if(i%5==0)
     {
      li.textContent = 'Buzz';
-   
     } 
      document.getElementById('number').appendChild(li);
   }
