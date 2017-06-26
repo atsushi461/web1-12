84b73d90e4ac3f624b69134758e47f1f23e7bf1a
atsushi461 <g1627030@st.t-kougei.ac.jp>
2017-06-26 17:46:24 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,18 +2,21 @@ document.getElementById('form').onsubmit = function(){
   window.alert(document.getElementById('form').word.value);
 };
 
-var Fizz_Buzz = fonction(number){
-  return Math.floor(
-    }
+var Fizz_Buzz = fonction(number)
+{
+  return num;
+}
     
-    for(var i =1; i <101; i++){
+    for(var i =1; i <101; i++)
+  {
     var li = document.createElement('li');
     var num = -1+i;
     li.textContent = num;
     document.getElementById('number').appendChild(li);
     
-    if(num%15==0){
-    li.textContent = 'FizzBuzz';
-    document.getElementById('number').appendChild(li);
+    if(num%15==0)
+    {
+     li.textContent = 'FizzBuzz';
+     document.getElementById('number').appendChild(li);
     }
   }
