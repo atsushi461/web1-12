8936c1a2b446f1378adad4fa60dc837d754b90db
atsushi461 <g1627030@st.t-kougei.ac.jp>
2017-06-26 17:51:00 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,12 +1,4 @@
-document.getElementById('form').onsubmit = function(){
-  window.alert(document.getElementById('form').word.value);
-};
-
-var Fizz_Buzz = fonction(number)
-{
-  return num;
-}
-    
+   
     for(var i =1; i <101; i++)
   {
     var li = document.createElement('li');
