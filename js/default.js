a03587b03ef148198fffa4160f528c3e9746b27d
atsushi461 <g1627030@st.t-kougei.ac.jp>
2017-06-26 17:42:59 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -8,8 +8,11 @@ var Fizz_Buzz = fonction(number){
     
     for(var i =1; i <101; i++){
     var li = document.createElement('li');
-    var number = -1+i;
-    if(number%15==0){
+    var num = -1+i;
+    li.textContent = num;
+    document.getElementById('number').appendChild(li);
+    
+    if(num%15==0){
     li.textContent = 'FizzBuzz';
     document.getElementById('number').appendChild(li);
     }
